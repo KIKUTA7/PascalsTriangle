d9c03a593d1e61656cc3d69481396a78e440a088
Helmut Seidl <helmut.seidl@tum.de>
2020-10-13 13:23:03 +0200

    Exercise-Template pushed by Artemis

---
+++ src/fop/w3pascal/Pascal.java
@@ -0,0 +1,13 @@
+package fop.w3pascal;
+
+class Pascal extends MiniJava {
+    // Function for computing the first n rows of Pascal's triangle.
+    public static int[][] pascal(int n) {
+	// TODO
+    }
+
+    public static void main(String[] args) {
+        // We assume that n >= 0 holds.
+	// TODO
+    }
+}
