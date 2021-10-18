aa351af53b52b6e107b36591b3155f887a3c1946
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-10-18 18:45:54 +0400

    no message

--- src/fop/w3pascal/Pascal.java
+++ src/fop/w3pascal/Pascal.java
@@ -3,11 +3,33 @@ package fop.w3pascal;
 class Pascal extends MiniJava {
     // Function for computing the first n rows of Pascal's triangle.
     public static int[][] pascal(int n) {
-	// TODO
+
+int b [][] = new int [n][n];
+b[0][0]=1;
+b[1][0]=1;
+b[1][1]=1;
+for (int i=2; i<n;i++)
+{
+    b[i][0] =1;
+    b[i][i] = 1;
+    for(int j=1;j<i;j++)
+        b[i][j] = b[i-1][j-1] + b[i-1][j];
+}
+return b;
     }
 
     public static void main(String[] args) {
-        // We assume that n >= 0 holds.
-	// TODO
+       int n=readInt();
+       int [][] a = pascal(n);
+       int i=1;
+       for (int k=0;k<n;k++)
+       {
+           for(int j=0;j<i;j++)
+           {  writeConsole(a[k][j] + " ");}
+           i++;
+           write("");
+       }
+
+
     }
 }
