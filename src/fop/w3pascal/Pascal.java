6ccc7811536ae4c65ee39f70a5ec67099897b389
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-10-20 00:43:50 +0400

    step 1

--- src/fop/w3pascal/Pascal.java
+++ src/fop/w3pascal/Pascal.java
@@ -4,33 +4,32 @@ class Pascal extends MiniJava {
     // Function for computing the first n rows of Pascal's triangle.
     public static int[][] pascal(int n) {
 
-int [][] b  = new int [n][n];
-for (int i=0; i<n;i++)
-{
-    b[i][0] =1;
-    b[i][i] = 1;
-    for(int j=1;j<i;j++)
-        b[i][j] = b[i-1][j-1] + b[i-1][j];
-}
-return b;
+        int[][] b = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            b[i][0] = 1;
+            b[i][i] = 1;
+            for (int j = 1; j < i; j++)
+                b[i][j] = b[i - 1][j - 1] + b[i - 1][j];
+        }
+        return b;
     }
 
     public static void main(String[] args) {
 
-       int n=read("Please insert number of rows:");
-              int [][] a = pascal(n);
-       int i=1;
-       for (int k=0;k<n;k++)
-       {
-           writeConsole("n=");
-           writeConsole(k);
-           writeConsole("\t");
-           for(int j=0;j<i;j++)
-           {  writeConsole(a[k][j]);
-           if(j!=i-1) writeConsole("\t");
-           else if(k!=n-1)writeConsole("\n");}
-           i++;
-       }
+        int n = read("Please insert number of rows:");
+        int[][] a = pascal(n);
+        int i = 1;
+        for (int k = 0; k < n; k++) {
+            writeConsole("n=");
+            writeConsole(k);
+            writeConsole("\t");
+            for (int j = 0; j < i; j++) {
+                writeConsole(a[k][j]);
+                if (j != i - 1) writeConsole("\t");
+                else if (k != n - 1) writeConsole("\n");
+            }
+            i++;
+        }
 
 
     }
